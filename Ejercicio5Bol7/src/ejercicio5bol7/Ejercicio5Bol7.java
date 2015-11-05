e79eb7a704cbcbb2f75a9db3bc1696149d53c82d
aabalalonso <aabalalonso@atanasoff15.danielcastelao.org>
2015-11-05 10:35:41 +0100

    commit ejercicio 5

---
+++ Ejercicio5Bol7/src/ejercicio5bol7/Ejercicio5Bol7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5bol7;
+
+/**
+ *
+ * @author aabalalonso
+ */
+public class Ejercicio5Bol7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Compara obx = new Compara ();
+        obx.setNum1();
+        obx.setNum2();
+        obx.setNum3();
+        obx.comparar();
+    }
+    
+}
